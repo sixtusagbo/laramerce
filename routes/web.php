e7eb897f8bb3747d2d2b7fd384f9d63ef6de0793
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-10-31 15:07:06 +0100

    Setup auth with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,28 @@
 <?php
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/products');
+    return view('welcome');
 });
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/dashboard', function () {
+    if (Auth::user()->role === 'admin') {
+        return redirect()->route('admin.dashboard');
+    }
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('products', ProductController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Cart routes
-Route::resource('cart', CartController::class)->only(['index', 'store']);
-Route::delete('/cart', [CartController::class, 'clear'])->name('cart.clear');
-Route::delete('/cart/{cart}/product/{product}', [CartController::class, 'remove'])->name('cart.remove');
+require __DIR__ . '/auth.php';
 
-// Route::get('/mimi/link-storage', function () {
-//     Artisan::call('storage:link');
-//     return 'success';
-// });
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
